16d20a455c99ed708ad795b063ee65e99ffe9c49
JonasPC <jonasdefarias@hotmail.com>
2015-07-25 06:19:51 -0300

    1.1.3

--- AmazonAPI/src/me/jonasxpx/AmazonMain.java
+++ AmazonAPI/src/me/jonasxpx/AmazonMain.java
@@ -61,8 +61,9 @@ public class AmazonMain {
 	 * @param instanceId Id da instancia
 	 */
 	public void startInstance(String instanceId){
+		String a = null;
 		while(true){
-			String a = ec2.describeInstances(new DescribeInstancesRequest().withInstanceIds(instanceId)).getReservations().get(0).getInstances().get(0).getState().getName();
+			a = ec2.describeInstances(new DescribeInstancesRequest().withInstanceIds(instanceId)).getReservations().get(0).getInstances().get(0).getState().getName();
 			System.out.println("Checando se esta padarado.... "+ a);
 			if(a.equalsIgnoreCase("stopped")){
 				ec2.startInstances(new StartInstancesRequest().withInstanceIds(instanceId));
@@ -84,11 +85,13 @@ public class AmazonMain {
 	 * @param ebs True para instancias acima da C3 e False Para instancias abaixo da C3
 	 */
 	public void chargeInstanceType(String instanceId,InstanceType type, boolean ebs){
+		String a = null;
+		ModifyInstanceAttributeRequest miar = null;
 		while(true){
-			String a = ec2.describeInstances(new DescribeInstancesRequest().withInstanceIds(instanceId)).getReservations().get(0).getInstances().get(0).getState().getName();
+			a = ec2.describeInstances(new DescribeInstancesRequest().withInstanceIds(instanceId)).getReservations().get(0).getInstances().get(0).getState().getName();
 			System.out.println("Checando se esta padarado.... "+ a);
 			if(a.equalsIgnoreCase("stopped")){
-				ModifyInstanceAttributeRequest miar = new ModifyInstanceAttributeRequest();
+				miar = new ModifyInstanceAttributeRequest();
 				miar.withInstanceId(instanceId);
 				miar.withInstanceType(type.nome);
 				ec2.modifyInstanceAttribute(miar);
@@ -112,12 +115,12 @@ public class AmazonMain {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		AmazonMain main = new AmazonMain(Region.getRegion(Regions.US_EAST_1));
 		if(args.length == 0){
 			System.out.println("Use <stop,start> <instanceId>");
 			System.out.println("Use <change> <type (t2medium,c4large,c4xlarge)> <instanceId>");
 			return;
 		}
+		AmazonMain main = new AmazonMain(Region.getRegion(Regions.US_EAST_1));
 		if(args.length == 2){
 			if(args[0].equalsIgnoreCase("stop")){
 				main.stopInstance(args[1]);
